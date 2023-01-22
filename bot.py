778ebd8bd2e8403e041b6fa2c1f35840b75cacf0
Syrup <ann200611@gmail.com>
2023-01-22 22:28:01 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands 
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='[', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    print(f"{member} join!")
+
+@bot.event
+async def on_member_remove(member):
+    print(f"{member} leave!")
+
+
+bot.run("MTA2NDc3MzQ3MzEyNzM2NjcwNw.Gp7cTs.iIdpxoC_URWvX_5gCGPgZYHyh7mgelJPYLA0P0")
